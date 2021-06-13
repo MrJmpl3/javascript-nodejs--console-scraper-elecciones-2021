0988888c64364234b37e243cc384a5624ea327ec
José Manuel Casani Guerra <jmpl3.soporte@gmail.com>
2021-06-12 22:43:38 -0500

    feat: first version

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # nodejs--console-scraper-elecciones-2021
 
 Este script esta creado para descargar y guardar los JSON de los datos de la mesas de la segundas elecciones presidenciales del 2021
+
+Para usar solamente necesitas:
+
+- Clonar el repositorio
+- Usar el comando `yarn install` para instalar las dependencias
+- Usar el comando `yarn run build` para compilar el codigo
+- Y finalmente ejecutas `yarn run start scraper --inicio=[mesa inicial] --final=[mesa final]` 
---
+++ index.ts
@@ -0,0 +1,72 @@
+import cac from "cac";
+import * as fs from "fs-extra";
+import puppeteer from "puppeteer";
+import userAgent from "user-agents";
+import path from "path";
+import delay from "delay";
+
+const isJson = (str) => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
+
+const mesaError = (mesa: string, error: string) => {
+  if (!fs.existsSync("error.csv")) {
+    fs.appendFileSync("error.csv", "MESA,ERROR\r\n");
+  }
+
+  fs.appendFileSync("error.csv", `${mesa},${error}\r\n`);
+};
+
+const cli = cac();
+
+cli
+  .command("scraper")
+  .option("--inicio [inicio]", "Mesa donde inicia")
+  .option("--final [final]", "Mesa donde finaliza")
+  .action(async ({ inicio, final }: { inicio: number; final: number }) => {
+    // console.log(inicio.toString().padStart(6, "0"));
+    // console.log(final.toString().padStart(6, "0"));
+
+    fs.ensureDirSync("json");
+
+    for (let mesa = inicio; mesa <= final; mesa++) {
+      const mesaPad = mesa.toString().padStart(6, "0");
+
+      console.log(`Scrapeando la mesa: ${mesaPad}`);
+
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.setUserAgent(userAgent.toString());
+      await page.goto(
+        `https://api.resultadossep.eleccionesgenerales2021.pe/mesas/detalle/${mesaPad}`
+      );
+
+      let element = await page.$("pre");
+      if (element === null) {
+        console.log(`Error en la mesa ${mesaPad} - No data`);
+        mesaError(mesaPad, "No data");
+        continue;
+      }
+
+      const data = await page.evaluate((el) => el.textContent, element);
+      if (!isJson(data)) {
+        console.log(`Error en la mesa ${mesaPad} - Data is not a JSON`);
+        mesaError(mesaPad, "Data is not a JSON");
+        continue;
+      }
+
+      console.log(`Guardando datos de la mesa: ${mesaPad}`);
+      fs.appendFileSync(path.resolve("json", `${mesaPad}.json`), data);
+
+      await delay(1000);
+
+      await browser.close();
+    }
+  });
+
+cli.parse();
